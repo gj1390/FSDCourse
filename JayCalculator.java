d59456f308fe7ad53a3c5bb8a55974365bf94dbe
gj1390 <jayram1361990@gmail.com>
2021-06-05 21:05:43 +0530

    Committing Calculator java program

---
+++ JayCalculator.java
@@ -0,0 +1,10 @@
+public class JayCalculator {
+public static void main(String[] args) {
+int a = Integer.parseInt(args[0]);
+int b = Integer.parseInt(args[1]);
+System.out.println("Addition of "+a+" "+b+" is: "+(a+b));
+System.out.println("Subtraction of "+a+" "+b+" is: "+(a-b));
+System.out.println("Multiplication of "+a+" "+b+" is: "+(a*b));
+System.out.println("Division of "+a+" "+b+" is: "+(a/b));
+}
+}
